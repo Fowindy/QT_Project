721a9a845fa50dc7c75ad65e724cdf0aa29a11e4
Fowindy <47002339@qq.com>
2020-12-02 11:02:13 +0800

    管理界面增加新员工

--- Src/Face_Recognition_System/AdminInfo.cpp
+++ Src/Face_Recognition_System/AdminInfo.cpp
@@ -6,6 +6,10 @@ AdminInfo::AdminInfo(QWidget *parent)
 {
 	ui = new Ui::AdminInfo();
 	ui->setupUi(this);
+	//实例化用户信息输入界面
+	m_infoInput = new infomationInput();
+	//连接用户输入界面的信号和槽
+	connect(m_infoInput, SIGNAL(adminInfoSignal()), this, SLOT(show()));
 }
 
 AdminInfo::~AdminInfo()
@@ -27,4 +31,20 @@ void AdminInfo::on_btnReturn_clicked()
 	this->hide();
 	//向首页发送信号
 	emit adminInfoSignal();
+}
+
+/************************************
+*@Method:    on_btnAddWorker_clicked
+*@Access:    private
+*@Returns:   void
+*@Author: 	  Fowindy
+*@Created:   2020/12/02 10:56
+*@Describe:	 增加员工按钮槽函数
+*************************************/
+void AdminInfo::on_btnAddWorker_clicked()
+{
+	//隐藏当前界面
+	this->hide();
+	//弹出用户信息输入界面
+	m_infoInput->show();
 }
--- Src/Face_Recognition_System/AdminInfo.h
+++ Src/Face_Recognition_System/AdminInfo.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include <QWidget>
+#include "infomationInput.h"
 namespace Ui { class AdminInfo; };
 
 class AdminInfo : public QWidget
@@ -13,9 +14,12 @@ public:
 
 private:
 	Ui::AdminInfo *ui;
+	infomationInput *m_infoInput;
 private slots:
 	//返回按钮槽函数
 	void on_btnReturn_clicked();
+	//增加员工按钮槽函数
+	void on_btnAddWorker_clicked();
 signals:
 	//向外界发射信号
 	void adminInfoSignal();
